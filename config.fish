cd27638e1e1a18b1c607e5bf076d7b48f6465099
Ian Hattendorf <ianhattendorf@gmail.com>
2017-03-24 19:28:53 -0700

    config.fish: test for fedora

--- config.fish
+++ config.fish
@@ -1,1 +1,5 @@
-set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+if test -e /etc/fedora-release
+	set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start | grep "^SSH_AUTH_SOCK" | awk -F "=" '{print $2}')
+else
+	set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+end
