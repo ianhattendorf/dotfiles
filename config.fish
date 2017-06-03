5e40370afc92545d9086b29f854c4d008efef1a0
Ian Hattendorf <ianhattendorf@gmail.com>
2017-06-02 17:07:04 -0700

    fish: use gpg-agent for ssh on fedora

--- config.fish
+++ config.fish
@@ -1,5 +1,12 @@
 if test -e /etc/fedora-release
-	set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start | grep "^SSH_AUTH_SOCK" | awk -F "=" '{print $2}')
+	# Set SSH_AUTH_SOCK
+	set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+
+	# Set GPG TTY
+	set -gx GPG_TTY (tty)
+
+	# Refresh gpg-agent tty in case user switches into an X session
+	gpg-connect-agent updatestartuptty /bye >/dev/null
 else
 	set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 end
