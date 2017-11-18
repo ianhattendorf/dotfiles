faf456d073301c75a2b314b4c82300963d42fd5a
Ian Hattendorf <ianhattendorf@gmail.com>
2017-11-18 12:44:53 -0700

    i3: fix nala touchpad tapping for real

--- config/i3/scripts/nala.sh
+++ config/i3/scripts/nala.sh
@@ -2,8 +2,10 @@
 
 HOSTNAME=`hostname`
 
-if [ "$HOSTNAME" = nala ]; then
+if [ "$HOSTNAME" = 'nala' ]; then
+  # get touchpad xinput id
+  TOUCHPAD_ID=`xinput list | grep 'Elantech Touchpad' | awk -F'=' '{print $2}' | grep -Po '^\d+'`
   # set touchpad Tapping Enabled
-  xinput set-prop 13 280 1
+  xinput set-prop $TOUCHPAD_ID 'libinput Tapping Enabled' 1
 fi
 
