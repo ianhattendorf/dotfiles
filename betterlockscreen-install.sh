3262e3a77120d31ac31884a13647dd31dbaae823
Ian Hattendorf <ian@ianhattendorf.com>
2018-05-08 16:15:37 -0700

    i3: Use wallpaper as background for i3lock

---
+++ betterlockscreen-install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eu
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+version="$1"
+dir="$HOME/bin"
+
+#url="https://github.com/pavanjadhaw/betterlockscreen/archive/$version.tar.gz"
+# Temporarily use fork until PR merged
+url="https://github.com/ianhattendorf/betterlockscreen/archive/$version.tar.gz"
+path="$dir/betterlockscreen"
+path_version="$path-$version"
+
+if [ ! -f "$path_version" ]; then
+  mkdir -p "$dir"
+  curl -L "$url" | tar xzv --strip-components=1 -C "$dir" "betterlockscreen-$version/betterlockscreen" && mv "$dir/betterlockscreen" "$path_version"
+  ln -s "$path_version" "$path" 
+fi
+
