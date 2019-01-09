2afae2d96c6686f68929ed893c00851ba812f9ff
Ian Hattendorf <ian@ianhattendorf.com>
2019-01-09 11:02:03 -0700

    emacs: Remove redundant comments

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,8 @@
 ;;; Configure defaults
 (customize-set-variable 'user-full-name "Ian Hattendorf")
 (customize-set-variable 'user-mail-address "ian@ianhattendorf.com")
+(customize-set-variable 'initial-scratch-message nil)
+(put 'upcase-region 'disabled nil)
 
 (add-to-list 'default-frame-alist '(font . "Fira Code 14"))
 
@@ -20,7 +22,6 @@
     (mac-auto-operator-composition-mode))
 
 ;;; Fira Code ligatures (requires Fira Code Symbol font, see: https://github.com/tonsky/FiraCode/issues/211#issuecomment-239058632)
-
 (require 'setup-ligatures)
 
 ;; Avoid polluting init.el with customizations
@@ -28,7 +29,7 @@
 (load custom-file :noerror)
 
 ;; Disable GUI elements
-;; Leave menu and tool bar enabled while learning
+;; Leave menu enabled
 (when (fboundp 'menu-bar-mode) (menu-bar-mode t))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
@@ -40,9 +41,6 @@
 (setq gc-cons-threshold (* 64 1000 1000))
 (add-hook 'after-init-hook #'(lambda ()
                                (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value)))))
-
-;; Scratch customization
-(customize-set-variable 'initial-scratch-message nil)
 
 ;; Tramp Mode (remote files)
 (setq tramp-default-method "ssh")
@@ -89,10 +87,11 @@
   (require 'use-package))
 (require 'bind-key)
 
-;;; Theme
 (load-theme 'leuven)
 
-;;; buffer-move
+(font-lock-add-keywords 'prog-mode
+                        '(("\\<\\(FIXME\\|TODO\\|BUG\\|XXX\\)" 1 font-lock-warning-face prepend)))
+
 (use-package buffer-move
   :ensure t
   :bind (
@@ -101,17 +100,14 @@
          ("<C-s-left>" . buf-move-left)
          ("<C-s-right>" . buf-move-right)))
 
-;;; whitespace-mode
 (use-package whitespace
   :init
   (dolist (hook '(prog-mode-hook text-mode-hook))
     (add-hook hook #'whitespace-mode))
-  ;; (add-hook 'before-save-hook #'whitespace-cleanup)
   :config
   (setq whitespace-line-column 120)
   (setq whitespace-style '(face tabs tab-mark empty trailing lines-tail)))
 
-;;; Projectile
 (use-package projectile
   :ensure t
   :config
@@ -140,7 +136,6 @@
               (neotree-find file-name)))
       (message "Could not find git project root."))))
 
-;;; NeoTree
 (use-package neotree
   :ensure t
   :bind (
@@ -149,31 +144,26 @@
   :config
   (setq neo-autorefresh nil))
 
-;;; Editorconfig
 (use-package editorconfig
   :ensure t
   :config
   (editorconfig-mode 1))
 
-;;; winum
 (use-package winum
   :ensure t
   :config
   (winum-mode))
 
-;;; smartparens
 (use-package smartparens
   :ensure t
   :config
   (use-package smartparens-config)
   (smartparens-global-mode 1))
 
-;;; rainbow-delimiters
 (use-package rainbow-delimiters
   :ensure t
   :hook (prog-mode . rainbow-delimiters-mode))
 
-;;; Counsel/Ivy
 (use-package counsel
   :ensure t
   :config
@@ -197,14 +187,14 @@
   :bind (("C-s" . swiper)
          ("C-r" . swiper)))
 
-;;; Drag Stuff
+; Disabled, unused
 (use-package drag-stuff
+  :disabled
   :ensure t
   :config
   (drag-stuff-define-keys)
   (drag-stuff-global-mode 1))
 
-;;; flyspell
 (use-package flyspell
   :bind (("<f8>" . flyspell-correct-word-generic)
          ("C-<f8>" . flyspell-correct-previous-word-generic)
@@ -223,7 +213,6 @@
 (use-package flyspell-correct-ivy
   :ensure t)
 
-;;; yasnippet
 (use-package yasnippet
   :ensure t
   :config
@@ -236,7 +225,6 @@
   :config
   (setq sh-basic-offset 2))
 
-;;; markdown-mode
 (use-package markdown-mode
   :ensure t
   :mode (("README\\.md\\'" . gfm-mode)
@@ -244,25 +232,21 @@
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
-;;; yaml-mode
 (use-package yaml-mode
   :ensure t
   :mode (".yml" ".yaml"))
 
-;;; json-mode
 (use-package json-mode
   :ensure t
   :mode (".json"))
 
-;;; meson-mode
-;; Disabled, https://github.com/wentasah/meson-mode/issues/16
+; Disabled, https://github.com/wentasah/meson-mode/issues/16
 (use-package meson-mode
   :disabled
   :ensure t
   :mode "\\meson\\.build\\'"
   :hook (meson-mode . company-mode))
 
-;;; Company
 (use-package company
   :ensure t
   :config
@@ -294,7 +278,6 @@
   (setq jedi:environment-virtualenv
         (list "virtualenv-3" "--system-site-packages")))
 
-;;; flycheck
 (use-package flycheck
   :ensure t
   :config
@@ -313,36 +296,10 @@
 (use-package flycheck-rtags
   :ensure t)
 
-;;; todo/fixme/etc. highlighting
-(font-lock-add-keywords 'prog-mode
-                        '(("\\<\\(FIXME\\|TODO\\|BUG\\|XXX\\)" 1 font-lock-warning-face prepend)))
-
-;; ;; tide
-;; (use-package tide
-;;   :ensure t
-;;   :after (typescript-mode company flycheck)
-;;   :hook ((typescript-mode . tide-setup)
-;;          (typescript-mode . tide-hl-identifier-mode)
-;;     (js2-mode . tide-setup)
-;;     (rjsx-mode . tide-setup)
-;;          (before-save . tide-format-before-save)))
-
-;; ;; rjsx
-;; (use-package rjsx-mode
-;;   :ensure t
-;;   :mode "\\.jsx$")
-
-;; ;; js2-mode
-;; (use-package js2-mode
-;;   :ensure t
-;;   :mode ("\\.js\\'" . js2-mode))
-
-;; javascript-mode
 (use-package js
   :config
   (setq js-indent-level 2))
 
-;; web-mode
 (use-package web-mode
   :ensure t
   :mode ("\\.jsx?\\'" "\\.hbs\\'")
@@ -357,7 +314,6 @@
   (add-to-list 'web-mode-indentation-params '("lineup-quotes" . nil))
   (add-to-list 'web-mode-indentation-params '("case-extra-offset" . nil)))
 
-;; clang-format
 (use-package clang-format
   :ensure t
   :config
@@ -366,13 +322,11 @@
     :config
     (define-key c-mode-base-map (kbd "C-c C-f") 'clang-format-region)))
 
-;;; Modern C++ font lock
 (use-package modern-cpp-font-lock
   :ensure t
   :config
   (modern-c++-font-lock-global-mode t))
 
-;;; RTags
 (use-package rtags
   :ensure t
   :config
@@ -381,16 +335,13 @@
   (setq rtags-completions-enabled t)
   (rtags-enable-standard-keybindings))
 
-;;; rpm-spec-mode
 (use-package rpm-spec-mode
   :ensure t)
 
-;;; diff-hl
 (use-package diff-hl
   :ensure t
   :init (global-diff-hl-mode t))
 
-;;; Magit
 (use-package magit
   :bind (("C-x g" . magit-status))
   :ensure t
@@ -403,25 +354,23 @@
           "^Yubikey for .*: ?$"
           "^Enter PIN for '.*': ?$")))
 
-;;; Magit Repositories
 (use-package magit-repos
   :config
   (setq magit-repository-directories '(("~/dotfiles" . 0) ("~/dev/repos" . 1))))
 
-;;; Magit Forge (wait until more mature)
-;; (use-package forge
-;;   :ensure t
-;;   :config
-;;   (delete "~/.authinfo.gpg" auth-sources)
-;;   (add-to-list 'auth-sources "~/.authinfo.gpg"))
+; Disabled (wait until more mature)
+(use-package forge
+  :disabled
+  :ensure t
+  :config
+  (delete "~/.authinfo.gpg" auth-sources)
+  (add-to-list 'auth-sources "~/.authinfo.gpg"))
 
-;;; Expand Region
 (use-package expand-region
   :ensure t
   :commands er/expand-region
   :bind ("C-=" . er/expand-region))
 
-;;; Random functions
 (defun toggle-window-split ()
   (interactive)
   (if (= (count-windows) 2)
@@ -449,12 +398,5 @@
 
 (global-set-key (kbd "C-x |") 'toggle-window-split)
 
-;; (load-file "~/.emacs.d/flow-for-emacs/flow.el")
-
-;; Company Flow
-;; (use-package company-flow
-;;   :ensure t)
-
 (provide 'init)
 ;;; init.el ends here
-(put 'upcase-region 'disabled nil)
