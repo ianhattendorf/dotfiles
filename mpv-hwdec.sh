b6935766d057e055305f8aeec139c451e4d54069
Ian Hattendorf <ian@ianhattendorf.com>
2019-01-28 22:57:47 -0700

    sway: Use swayidle/swaylock for locking

--- mpv-hwdec.sh
+++ mpv-hwdec.sh
@@ -3,5 +3,11 @@
 HOSTNAME=`hostname --short`
 
 # Use vaapi-copy for Wayland
-sh -c 'if [ "$HOSTNAME" = ''nala'' ]; then echo ''hwdec=vaapi-copy''; else echo ''hwdec=auto''; fi > ~/.config/mpv/hwdec.conf'
-
+# Also set gpu-context
+if [ "$HOSTNAME" = 'nala' ]; then
+  echo 'hwdec=vaapi-copy
+gpu-context=wayland
+'
+else
+  echo 'hwdec=auto'
+fi > ~/.config/mpv/hwdec.conf
