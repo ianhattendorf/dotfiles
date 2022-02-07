c9986bdfa929b1c990186bc18b55bd3ee767d062
Ian Hattendorf <ianh@axosoft.com>
2022-02-07 08:44:20 -0700

    gitconfig: update work

--- gitconfig-local.sh
+++ gitconfig-local.sh
@@ -7,6 +7,6 @@ if [ $(uname) == 'Darwin' ]; then
 fi
 
 basedir=$(dirname "$( readlink -f -- "$0")")
-[ $(hostname) == 'ianh-mbp.axonet.local' ] && config_type='work' || config_type='home'
+[ $(hostname) == 'ianh-dell.axonet.local' ] && config_type='work' || config_type='home'
 
 ln -fs "$basedir/.gitconfig-local-$config_type" ~/.gitconfig-local
