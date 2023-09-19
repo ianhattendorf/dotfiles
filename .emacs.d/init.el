5f056eb134846922863a1736102acff0f9cbf21b
Ian Hattendorf <ian.hattendorf@gitkraken.com>
2023-09-18 17:33:47 -0700

    emacs: use-package emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,114 +6,103 @@
 
 ;;; Code:
 
-;;; Configure defaults
-(customize-set-variable 'user-full-name "Ian Hattendorf")
-(customize-set-variable 'user-mail-address "ian@ianhattendorf.com")
-(customize-set-variable 'initial-scratch-message nil)
-
-;; Avoid polluting init.el with customizations
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file :noerror)
-
-;; Disable GUI elements
-;; Leave menu enabled
-(when (fboundp 'menu-bar-mode) (menu-bar-mode t))
-;(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
-;; Disable splash screen
-(setq inhibit-startup-screen t)
-
-;; Show matching parens
-(show-paren-mode 1)
-(setq show-paren-delay 0)
-
-;; Spaces instead of tabs
-(setq-default indent-tabs-mode nil)
-
-;; Disable bell
-(setq ring-bell-function #'ignore)
-
-;; Smooth scrolling
-(setq pixel-scroll-precision-large-scroll-height 40.0)
-(setq pixel-scroll-precision-interpolate-page t)
-(pixel-scroll-precision-mode)
-
-;; Display line infomation
-(global-display-line-numbers-mode 1)
-(line-number-mode 1)
-(column-number-mode 1)
-
-;; Native compilation
-(setq native-comp-async-report-warnings-errors 'silent)
-(setq native-compile-prune-cache t)
-
-;; Backups/autosave
-(setq make-backup-files nil)
-
-;; Git symbolic link visit real file
-(setq vc-follow-symlinks t)
-
-(global-set-key (kbd "s-u") 'revert-buffer)
-(global-unset-key (kbd "C-z"))
-
-;; Auto revert buffer if unchanged and file on disk changes
-(global-auto-revert-mode)
-
-;;; Tree-sitter
-; Requires branch, not sha
-;(setq treesit-language-source-alist
-;      '((bash "https://github.com/tree-sitter/tree-sitter-bash" "c0f5797a728fc4ebd78a8b0e436b1494a8ab5f51") ; 0.20.0
-;        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "f1e5a09b8d02f8209a68249c93f0ad647b228e6e") ; 0.20.1
-;        (json "https://github.com/tree-sitter/tree-sitter-json" "ca3f8919800e3c1ad4508de3bfd7b0b860ce434f") ; master-20230710
-;        (markdown "https://github.com/MDeiml/tree-sitter-markdown" "aaf76797aa8ecd9a5e78e0ec3681941de6c945ee") ; 0.1.6
-;        (toml "https://github.com/tree-sitter/tree-sitter-toml" "342d9be207c2dba869b9967124c679b5e6fd0ebe") ; master-20220421
-;        (rust "https://github.com/tree-sitter/tree-sitter-rust" "2697585ee0a6a07a9c762d8855022f974c831a89") ; 0.20.4
-;        (yaml "https://github.com/ikatyang/tree-sitter-yaml" "0e36bed171768908f331ff7dff9d956bae016efb"))) ; master-20210510
-
-; M-: (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
-(setq treesit-language-source-alist
-      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-        (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
-        (json "https://github.com/tree-sitter/tree-sitter-json")
-        (markdown "https://github.com/MDeiml/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src")
-        (rust "https://github.com/tree-sitter/tree-sitter-rust")
-        (toml "https://github.com/tree-sitter/tree-sitter-toml")
-        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
-
-;;; Packages
-; dnf install emacs-json-mode emacs-magit emacs-yaml-mode
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/yaml-mode/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/json-mode/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/json-snatcher/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/magit/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/async/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/dash/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/with-editor/")
-
-(setq package-enable-at-startup nil)
-
-;; straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/d4cd480395f3d593e1987fbe7b26b38cd4918b9c/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
 ;;; use-package
 (eval-when-compile
   (require 'use-package))
 
 (use-package emacs
   :init
+  ;; Tree-sitter
+   (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (markdown "https://github.com/MDeiml/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+  ;; Packages
+  ; dnf install emacs-json-mode emacs-magit emacs-yaml-mode
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/yaml-mode/")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/json-mode/")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/json-snatcher/")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/magit/")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/async/")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/dash/")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/with-editor/")
+
+  (setq package-enable-at-startup nil)
+
+  ;; straight.el
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 6))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/d4cd480395f3d593e1987fbe7b26b38cd4918b9c/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+
+  ;; Configure defaults
+  (customize-set-variable 'user-full-name "Ian Hattendorf")
+  (customize-set-variable 'user-mail-address "ian@ianhattendorf.com")
+  (customize-set-variable 'initial-scratch-message nil)
+
+  ;; Avoid polluting init.el with customizations
+  (setq custom-file "~/.emacs.d/custom.el")
+  (load custom-file :noerror)
+
+  ;; Disable GUI elements
+  ;; Leave menu enabled
+  (when (fboundp 'menu-bar-mode) (menu-bar-mode t))
+                                        ;(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+                                        ;(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+  ;; Disable splash screen
+  (setq inhibit-startup-screen t)
+
+  ;; Native compilation
+  (setq native-comp-async-report-warnings-errors 'silent)
+  (setq native-compile-prune-cache t)
+
+  ;; Backups/autosave
+  (setq make-backup-files nil)
+
+  ;; Git symbolic link visit real file
+  (setq vc-follow-symlinks t)
+
+  (global-set-key (kbd "s-u") 'revert-buffer)
+  (global-unset-key (kbd "C-z"))
+
+  ;; Auto revert buffer if unchanged and file on disk changes
+  (global-auto-revert-mode)
+
+  ;; Show matching parens
+  (show-paren-mode 1)
+  (setq show-paren-delay 0)
+
+  ;; Spaces instead of tabs
+  (setq-default indent-tabs-mode nil)
+
+  ;; Disable bell
+  (setq ring-bell-function #'ignore)
+
+  ;; Smooth scrolling
+  (setq pixel-scroll-precision-large-scroll-height 40.0)
+  (setq pixel-scroll-precision-interpolate-page t)
+  (pixel-scroll-precision-mode)
+
+  ;; Display line infomation
+  (global-display-line-numbers-mode 1)
+  (line-number-mode 1)
+  (column-number-mode 1)
+
   ;; TAB cycle if there are only few candidates
   (setq completion-cycle-threshold 3)
 
